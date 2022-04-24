af05768c95f091aa7244db72a3abb61cf631a5cc
aakansha1234 <20503818+aakansha1234@users.noreply.github.com>
2022-04-24 06:31:04 +0530

    split contract

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,27 +1,36 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract YourContract {
+contract YourContract is Ownable {
 
-  event SetPurpose(address sender, string purpose);
+  address payable public other;
+  uint256 public basis; // out of 10_000
+  constructor() {}
 
-  string public purpose = "Building Unstoppable Apps!!!";
-
-  constructor() payable {
-    // what should we do on deploy?
+  function setOther(address _other) external {
+    other = payable(_other);
   }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
+  function setBasis(uint256 _basis) external {
+    basis = _basis;
   }
 
-  // to support receiving ETH by default
-  receive() external payable {}
+  function distributeErc20(address erc20) external {
+    uint256 bal = IERC20(erc20).balanceOf(address(this));
+    IERC20(erc20).transferFrom(address(this), other, bal * basis / 10_000);
+    IERC20(erc20).transferFrom(address(this), owner(), IERC20(erc20).balanceOf(address(this)));
+  }
+
+  function distributeEth() external {
+    uint256 bal = address(this).balance;
+    address(other).call{value: bal * basis / 10_000}("");
+    owner().call{value: address(this).balance}("");
+  }
+
   fallback() external payable {}
+  receive() external payable {}
 }
